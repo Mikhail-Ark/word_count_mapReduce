fae5ff9e008c70c73ce501fab1fcad1862aec611
Mikhail Ark <mikhail.ark@yandex.ru>
2022-05-03 16:14:57 +0300

    reduce over sorted

--- worker/tasks/reduce.py
+++ worker/tasks/reduce.py
@@ -5,8 +5,6 @@ from worker.utils import make_text_generator, write_output
 
 
 def word_count_reduce(input_path, output_path, job_id, merge_sort=False):
-    # TODO: implement merge_sort
-    assert not merge_sort, "merge_sort is not implemented"
     input_files_paths = find_files_to_reduce(input_path, job_id)
     words = make_text_generator(input_files_paths, merge_sort)
     counter_items = count(words, merge_sort)
@@ -22,9 +20,26 @@ def find_files_to_reduce(input_path, job_id=0):
 
 
 def count(words, sorted=False):
-    assert not sorted, "count sorted is not implemented"
-    counter = Counter(words)
-    return counter.items()
+    if sorted:
+        try:
+            current_word = next(words)
+        except StopIteration:
+            return []
+        if not current_word:
+            return []
+        n = 1
+        for word in words:
+            if word == current_word:
+                n += 1
+            else:
+                yield (current_word, n)
+                current_word = word
+                n = 1
+        yield (current_word, n)
+
+    else:
+        counter = Counter(words)
+        return (item for item in counter.items())
 
 
 def items_to_str(items):
