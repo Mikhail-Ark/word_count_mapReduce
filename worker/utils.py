5ed0c8a20c1c8a675a24bae7a637897559b93d97
Mikhail Ark <mikhail.ark@yandex.ru>
2022-05-03 14:38:53 +0300

    chain to text generator

--- worker/utils.py
+++ worker/utils.py
@@ -1,17 +1,18 @@
+from itertools import chain
 
 
 def make_text_generator(input_file_path, merge_sort=False):
     assert not merge_sort, "merge_sort is not implemented"
     if isinstance(input_file_path, str):
-        for line in make_text_generator_single(input_file_path):
-            yield line
+        return make_text_generator_single(input_file_path)
 
     elif isinstance(input_file_path, list):
         assert all(isinstance(x, str) for x in input_file_path), \
         "file path should be str"
-        for file_path in input_file_path:
-            for line in make_text_generator_single(file_path):
-                yield line
+        return chain(
+            make_text_generator_single(file_path) \
+            for file_path in input_file_path
+        )
     else:
         raise AssertionError("unknown path obj type")
 
