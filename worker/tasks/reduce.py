c293d332d15b7c27dd85d8837115de3ffa7e6b83
Mikhail Ark <mikhail.ark@yandex.ru>
2022-05-03 14:26:38 +0300

    basic reducer added

---
+++ worker/tasks/reduce.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from os import listdir
+
+from worker.utils import make_text_generator, write_output
+
+
+def word_count_reduce(input_path, output_path, job_id, merge_sort=False):
+    # TODO: implement merge_sort
+    assert not merge_sort, "merge_sort is not implemented"
+    input_files_paths = find_files_to_reduce(input_path, job_id)
+    words = make_text_generator(input_files_paths, merge_sort)
+    counter_items = count(words, merge_sort)
+    write_output(items_to_str(counter_items), f"{output_path}-{job_id}")
+    
+
+def find_files_to_reduce(input_path, job_id=0):
+    file_name_ending = f"-{job_id}"
+    return [
+        input_path + file_name for file_name in listdir(input_path)
+        if file_name.endswith(file_name_ending)
+    ]
+
+
+def count(words, sorted=False):
+    assert not sorted, "count sorted is not implemented"
+    counter = Counter(words)
+    return counter.items()
+
+
+def items_to_str(items):
+    return (f"{word} {n}" for word, n in items)
