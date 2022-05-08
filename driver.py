afdfbf4069971b5199430cd3c090c7d42fd8dbf5
Mikhail Ark <mikhail.ark@yandex.ru>
2022-05-08 13:37:02 +0300

    driver logging + argparse

--- driver.py
+++ driver.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 import grpc
@@ -5,12 +6,14 @@ import wordcount_mr_pb2
 import wordcount_mr_pb2_grpc
 
 
-def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
+def run(n_map, n_reduce):
+    logging.info(f"run with n_map={n_map}, n_reduce={n_reduce}")
+    with grpc.insecure_channel("localhost:50051") as channel:
         stub = wordcount_mr_pb2_grpc.WordCountMRStub(channel)
         task = make_map_task()
+        logging.info("send task")
         response = stub.DoTask(task)
-    print("Driver received: " + str(response.success))
+        logging.info(f"success: {str(response.success)}")
 
 
 def make_map_task():
@@ -26,6 +29,19 @@ def make_map_task():
     )
 
 
-if __name__ == '__main__':
-    logging.basicConfig()
-    run()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--n_map", "-N", type=int, nargs="?", const=1, default=1,
+        help="number of map tasks"
+    )
+    parser.add_argument(
+        "--n_reduce", "-M", type=int, nargs="?", const=1, default=1,
+        help="number of reduce tasks"
+    )
+    args = parser.parse_args()
+    logging.basicConfig(
+        filename="./logs/driver.log", level=logging.DEBUG,
+        format="%(asctime)s %(message)s", filemode='w'
+    )
+    run(n_map=args.n_map, n_reduce=args.n_reduce)
